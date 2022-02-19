c52323f65c19a63fb0bd6c512ef8a8541d234696
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-02-19 16:52:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
+import React, { useState } from "react";
 
 function App() {
+  const state = {
+    courses: [{ name: "html" }, { name: "css" }, { name: "jQuery" }],
+    current: "",
+  };
+  const [current, setCurrent] = useState(state.current);
+  const [Courses, setCourses] = useState(state.courses);
+  //update course
+  const updateCourse = (e) => {
+    setCurrent(e.target.value);
+  };
+
+  //add course
+  const addCourse = (e) => {
+    e.preventDefault();
+    // let current = state.current;
+    // let courses = state.courses;
+    courses.push({ name: current });
+    setCourses(Courses);
+    setCurrent("");
+    console.log("course add");
+    console.log(current);
+  };
+  const { courses } = state;
+  const courseList = courses.map((course, index) => {
+    return <CourseList details={course} key={index} />;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <h2>Add course</h2>
+
+      <CourseForm
+        current={state.current}
+        updateCourse={updateCourse}
+        addCourse={addCourse}
+      />
+      <ul>{courseList}</ul>
+    </section>
   );
 }
 
